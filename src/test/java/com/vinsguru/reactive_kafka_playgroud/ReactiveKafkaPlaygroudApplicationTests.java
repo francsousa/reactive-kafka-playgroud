1d90f3ed6cbfc980cf1ba31781265b141af10ee0
Francisco Sousa <franciscoeds1987@hotmail.com>
2025-11-30 10:22:17 -0300

    Project Setup

---
+++ src/test/java/com/vinsguru/reactive_kafka_playgroud/ReactiveKafkaPlaygroudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinsguru.reactive_kafka_playgroud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaPlaygroudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
