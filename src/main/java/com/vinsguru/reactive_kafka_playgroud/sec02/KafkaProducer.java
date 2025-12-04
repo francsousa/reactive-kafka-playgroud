9983b349d02ed32e82b390c911c358474be25fe0
Francisco Sousa <franciscoeds1987@hotmail.com>
2025-12-04 07:57:44 -0300

    Creating Sender Record

--- src/main/java/com/vinsguru/reactive_kafka_playgroud/sec02/KafkaProducer.java
+++ src/main/java/com/vinsguru/reactive_kafka_playgroud/sec02/KafkaProducer.java
@@ -1,16 +1,24 @@
 package com.vinsguru.reactive_kafka_playgroud.sec02;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
 
+import java.time.Duration;
 import java.util.Map;
 
 /*
     goal: to demo a simple Kafka producer using reactor-kafka
 */
 public class KafkaProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
 
     public static void main(String[] args) {
 
@@ -20,10 +28,17 @@ public class KafkaProducer {
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
         );
 
-        var options = SenderOptions.create(producerConfig);
+        var options = SenderOptions.<String, String>create(producerConfig);
+
+        var flux = Flux.interval(Duration.ofMillis(100))
+                .take(100)
+                .map(i -> new ProducerRecord<>("order-events", i.toString(), "order-" + i))
+                .map(pr -> SenderRecord.create(pr, pr.key()));
 
         KafkaSender.create(options)
-                .send(null);
+                .send(flux)
+                .doOnNext(r -> log.info("correlation id: {}", r.correlationMetadata()))
+                .subscribe();
     }
 
 }
