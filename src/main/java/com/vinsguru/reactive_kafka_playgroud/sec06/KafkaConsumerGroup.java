4c4c8b58c217810525ad5dcb7595d265d731bbad
Francisco Sousa <franciscoeds1987@hotmail.com>
2025-12-07 15:20:11 -0300

    Range Assignor

--- src/main/java/com/vinsguru/reactive_kafka_playgroud/sec06/KafkaConsumerGroup.java
+++ src/main/java/com/vinsguru/reactive_kafka_playgroud/sec06/KafkaConsumerGroup.java
@@ -5,6 +5,14 @@ package com.vinsguru.reactive_kafka_playgroud.sec06;
 */
 public class KafkaConsumerGroup {
 
+    /*
+    * RangeAssignor
+    *
+    * 0, 1, 2
+    *
+    * 1, 2, 3
+    * */
+
     private static class Consumer1 {
         public static void main(String[] args) {
             KafkaConsumer.start("1");
