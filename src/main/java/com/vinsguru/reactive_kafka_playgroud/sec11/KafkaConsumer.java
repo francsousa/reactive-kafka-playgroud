4c7c8d6c57e2d5b447481bc74eaa229422db1378
Francisco Sousa <franciscoeds1987@hotmail.com>
2025-12-19 08:36:53 -0300

    GroupedFlux - Parallel and Message Ordering - Part 2

--- src/main/java/com/vinsguru/reactive_kafka_playgroud/sec11/KafkaConsumer.java
+++ src/main/java/com/vinsguru/reactive_kafka_playgroud/sec11/KafkaConsumer.java
@@ -40,6 +40,8 @@ public class KafkaConsumer {
         KafkaReceiver.create(options)
                 .receive()
                 .groupBy(r -> Integer.parseInt(r.key()) % 5) // just for demo
+                // we can also group by r.partition()
+                // r.key().hashCode() % 5
                 .flatMap(KafkaConsumer::batchProcess)
                 .subscribe();
     }
